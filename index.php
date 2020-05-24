d29edd1f746f2070515e348c3136fd7e43fa19dc
nox-0202 <fernandinho.nco@gmail.com>
2020-05-24 18:23:12 -0300

    file structure and first real-time test

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  require __DIR__ . '/vendor/autoload.php';
+
+  $pusher = new Pusher\Pusher(
+    PUSHER_API_KEYS['key'],
+    PUSHER_API_KEYS['secret'],
+    PUSHER_API_KEYS['app_id'],
+    PUSHER_API_KEYS['options']
+  );
+
+  $data['message'] = 'hello world';
+  $pusher->trigger('my-channel', 'my-event', $data);
+
+  include __DIR__ . "/views/pusher.html";
+
+?>
+
---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php 
+
+    define('PUSHER_API_KEYS', [
+        "app_id" => "967683",
+        "key" => "7c66e54adcd6eb6bb536",
+        "secret" => "8df130a33c34f5336a20",
+        "cluster" => "us2",
+        "options" => array(
+            'cluster' => 'us2',
+            'useTLS' => true
+        )
+    ]);
+    
