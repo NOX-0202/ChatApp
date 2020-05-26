3a1ce96ce3f537c05ac002dcad1c99c774d2f35c
nox-0202 <fernandinho.nco@gmail.com>
2020-05-26 16:40:27 -0300

    first chat consept

--- index.php
+++ index.php
@@ -1,17 +1,2 @@
 <?php
   require __DIR__ . '/vendor/autoload.php';
-
-  $pusher = new Pusher\Pusher(
-    PUSHER_API_KEYS['key'],
-    PUSHER_API_KEYS['secret'],
-    PUSHER_API_KEYS['app_id'],
-    PUSHER_API_KEYS['options']
-  );
-
-  $data['message'] = 'hello world';
-  $pusher->trigger('my-channel', 'my-event', $data);
-
-  include __DIR__ . "/src/examples/pusher.html";
-
-?>
-
